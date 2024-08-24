963701a543ab5a84b82ebbb627faac398b8db78d
unknown <cogitaciones1@gmail.com>
2024-08-24 03:02:12 -0300

    new commit

--- app/epic/data.js
+++ app/epic/data.js
@@ -1,5 +1,5 @@
 
-export const dataEpic =  {mainComponent:{title:'EPIC',text1:"The EPIC API provides information on the daily imagery collected by DSCOVR's Earth Polychromatic Imaging Camera (EPIC) instrument.",text2:'Uniquely positioned at the Earth-Sun Lagrange point, EPIC provides full disc imagery of the Earth and captures unique perspectives of certain astronomical events such as lunar transits using a 2048x2048 pixel CCD (Charge Coupled Device) detector coupled to a 30-cm aperture Cassegrain telescope. '}}
+export const dataEpic =  {mainComponent:{title:'EPIC',text1:"The EPIC API provides information on the daily imagery collected by DSCOVR's Earth Polychromatic Imaging Camera (EPIC) instrument."}}
 
 
  
