f445a3905273ed2eba1ff55b6af828dd0298a7a9
unknown <cogitaciones1@gmail.com>
2024-08-26 19:02:59 -0300

    m

--- app/apod/rangeDate/page.jsx
+++ app/apod/rangeDate/page.jsx
@@ -31,7 +31,7 @@ function RangeDate() {
 
   useEffect(() => {
     if (typeof window !== "undefined") {
-      setData(JSON.parse(sessionStorage.getItem("NASA-apod")) || []);
+      setData(JSON.parse(sessionStorage.getItem("NASA-pictures")) || []);
     }
   }, []);
   const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
